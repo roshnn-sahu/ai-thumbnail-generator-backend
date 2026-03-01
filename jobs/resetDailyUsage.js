9c76fd3c11898929a20e7f558b5ac5dcec4d1f3d
roshann <locroshan@gmail.com>
2026-03-01 18:53:58 +0530

    feat: add daily cron job to reset credits and generation count for free plan users

--- jobs/resetDailyUsage.js
+++ jobs/resetDailyUsage.js
@@ -1,13 +1,14 @@
+
 import cron from "node-cron";
 import userModel from "../models/userModel.js";
 
 //0 0 * * *  for 12am night This runs every day at 12:00 AM
 cron.schedule("0 0 * * *", async () => {
     try {
-        console.log("Running midnight credit reset...");
+        console.log("Running midnight credit reset for free plan users...");
 
         await userModel.updateMany(
-            {},
+            { plan: "free" },
             {
                 $set: {
                     credits: 3,
@@ -17,7 +18,7 @@ cron.schedule("0 0 * * *", async () => {
             }
         );
 
-        console.log("Daily credits reset successfully.");
+        console.log("Daily credits reset successfully for free plan users.");
     } catch (error) {
         console.error("Midnight reset failed:", error);
     }
