62a3848af70cc987108acd51b03bff08f6742760
roshann <locroshan@gmail.com>
2026-03-02 17:24:43 +0530

    feat: add initial MongoDB connection setup in `db.js`.

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 export const connectDB = async () => {
 
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Database connected");
     } catch (error) {
         console.log(error);
