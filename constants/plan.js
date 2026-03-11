1dbe8c3d8824fa74fed210f2610729ed7b965dfc
codewithroshann <locroshan@gmail.com>
2026-03-11 17:33:38 +0530

    feat: add plan configurations for offline and online environments based on production status

--- constants/plan.js
+++ constants/plan.js
@@ -1,4 +1,6 @@
-export const PLANS = {
+const isProduction = true;
+
+const OFFLINE = {
   plan_SNuqOid3vopvhN: {
     razorpayPlanId: "plan_SNuqOid3vopvhN",
     internalId: "pro",
@@ -43,3 +45,50 @@ export const PLANS = {
     unlimited: true,
   },
 };
+const ONLINE = {
+  plan_SPs00Xh2kyEAJ6: {
+    razorpayPlanId: "plan_SPs00Xh2kyEAJ6",
+    internalId: "pro",
+    name: "Pro",
+    price: 599,
+    duration: "monthly",
+    days: 30,
+    credits: 300,
+    unlimited: false,
+  },
+
+  plan_SP4bUyJvcgRsBE: {
+    razorpayPlanId: "plan_SP4bUyJvcgRsBE",
+    internalId: "pro",
+    name: "Pro",
+    price: 4999,
+    duration: "yearly",
+    days: 365,
+    credits: 300,
+    unlimited: false,
+  },
+
+  plan_SP4bVcWrMnarXV: {
+    razorpayPlanId: "plan_SP4bVcWrMnarXV",
+    internalId: "creator",
+    name: "Creator",
+    price: 999,
+    duration: "monthly",
+    days: 30,
+    credits: null,
+    unlimited: true,
+  },
+
+  plan_SP4bWDFkgr0Jrr: {
+    razorpayPlanId: "plan_SP4bWDFkgr0Jrr",
+    internalId: "creator",
+    name: "Creator",
+    price: 8999,
+    duration: "yearly",
+    days: 365,
+    credits: null,
+    unlimited: true,
+  },
+};
+
+export const PLANS = isProduction ? ONLINE : OFFLINE;
