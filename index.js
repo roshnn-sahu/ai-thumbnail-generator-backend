91a8028a0446e6687920ab1a5aeae55887bc6c9b
roshnn-sahu <locroshan@gmail.com>
2026-07-01 14:34:58 +0530

    feat: increase request body size limits and add environment variable template file

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ connectDB();
 
 const app = express();
 
-// ✅ CORS must be first — before any routes
+
 const allowedOrigins = [
   process.env.FRONTEND_URL || "http://localhost:3000",
   "https://ai-thumbnail-generator-five.vercel.app",
@@ -38,13 +38,9 @@ app.use(
 );
 
 app.use("/api/subscription", subscriptionRoutes);
-<<<<<<< Updated upstream
-app.use(express.json());
-=======
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
->>>>>>> Stashed changes
 app.use(helmet());
 app.use(morgan("dev"));
 
